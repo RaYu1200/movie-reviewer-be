6e492e870c559521b3c855bebea531ebc5f3c3b4
Yuvrajsinh Raulji <yuvrajsinhraulji@Yuvrajsinhs-MacBook-Air.local>
2024-08-04 01:57:37 -0700

    backend code init

---
+++ src/main/java/dev/RaYu1200/movie_reviewer/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.RaYu1200.movie_reviewer;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ src/test/java/dev/RaYu1200/movie_reviewer/MovieReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.RaYu1200.movie_reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
