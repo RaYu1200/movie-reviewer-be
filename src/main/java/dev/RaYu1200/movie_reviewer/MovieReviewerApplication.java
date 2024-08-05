374b71a46feb4fa472080f57e32c740fe7c2102f
Yuvrajsinh Raulji <yuvrajsinhraulji@Yuvrajsinhs-MacBook-Air.local>
2024-08-05 01:42:02 -0700

    CORS allowed

--- src/main/java/dev/RaYu1200/movie_reviewer/MovieReviewerApplication.java
+++ src/main/java/dev/RaYu1200/movie_reviewer/MovieReviewerApplication.java
@@ -2,19 +2,11 @@ package dev.RaYu1200.movie_reviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MovieReviewerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieReviewerApplication.class, args);
-	}
-
-	@GetMapping("/healthCheck")
-	public String apiHealthCheck() {
-		return "The server is up and running!";
 	}
 }
--- src/main/java/dev/RaYu1200/movie_reviewer/ReviewController.java
+++ src/main/java/dev/RaYu1200/movie_reviewer/ReviewController.java
@@ -1,5 +1,6 @@
 package dev.RaYu1200.movie_reviewer;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
